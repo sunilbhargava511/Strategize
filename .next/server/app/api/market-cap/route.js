f55b93a2462c4b24f9fe2fa9edc7349d66ba9d90
Sunil Bhargava <sunilbhargava@Sunils-MacBook-2022.local>
2025-07-18 14:56:37 -0700

    Add .next to gitignore

--- .next/server/app/api/market-cap/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/market-cap/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_next_6c7a8cbe._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__04872cf2._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/market-cap/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/market-cap/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/market-cap/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"40NSA4BYaP5tZJAcClMfG9K2Ci9LlMN5aLvJS5O9Oqw=\"\n}"
